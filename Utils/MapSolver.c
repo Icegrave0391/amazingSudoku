fecbdc5e69d059c438ec831ecba56b11affdefbe
Immortalise <31989262+Immortalise@users.noreply.github.com>
2019-10-08 16:46:06 +0800

    add solver

---
+++ Utils/MapSolver.c
@@ -0,0 +1,9 @@
+//
+//  MapSolver.c
+//  amazingSudoku
+//
+//  Created by Triste on 2019/10/8.
+//  Copyright © 2019 icegrave0391. All rights reserved.
+//
+
+#include "MapSolver.h"
---
+++ Utils/MapSolver.h
@@ -0,0 +1,14 @@
+//
+//  MapSolver.h
+//  amazingSudoku
+//
+//  Created by Triste on 2019/10/8.
+//  Copyright © 2019 icegrave0391. All rights reserved.
+//
+
+#ifndef MapSolver_h
+#define MapSolver_h
+
+#include <stdio.h>
+
+#endif /* MapSolver_h */
